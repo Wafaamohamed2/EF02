d5a659b67abd413cfe4b0b701cc66be76b3f472d
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2024-08-25 12:17:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var configration = new ConfigurationBuilder()
+           .AddJsonFile("appsitting.json")
+           .Build();
+
+
+        //read from user input
+        var walletToInsert = new Wallet
+        {
+            Holder = "Menaa",
+            Balance = 5500
+        };
+
+
+
+        SqlConnection connction = new SqlConnection(configration.GetSection("constr").Value);
+        var sql = "INSERT INTO WALLETS (Holder ,Balance) VALUES " +
+            $"(@Holder , @Balance)";
+
+        SqlParameter holderParameter = new SqlParameter
+        {
+            ParameterName ="@Holder",
+            SqlDbType = SqlDbType.VarChar,
+            Value = walletToInsert.Holder,
+            Direction = ParameterDirection.Input,
+
+        };
+
+
+        SqlParameter balanceParameter = new SqlParameter
+        {
+            ParameterName = "@Balance",
+            SqlDbType = SqlDbType.Decimal,
+            Value = walletToInsert.Balance,
+            Direction = ParameterDirection.Input,
+
+        };
+
+
+        SqlCommand cmd = new SqlCommand(sql, connction);
+
+        cmd.Parameters.Add(holderParameter);
+        cmd.Parameters.Add(balanceParameter);
+        cmd.CommandType = CommandType.Text;
+
+        connction.Open();
+
+        if (cmd.ExecuteNonQuery() > 0)
+        {
+            Console.WriteLine($"wallet for {walletToInsert.Holder} added successfuly ");
+        }
+        else {
+
+            Console.WriteLine($"Error : wallet for  {walletToInsert.Holder}");
+        }
+      
+        connction.Close();
+
+    }
+}
